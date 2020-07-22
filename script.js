a1e521ca9648624cb5fdeecd98ff1ee0e64d808e
Michael Carr <mcarr2468@gmail.com>
2020-07-22 16:45:22 -0500

    v1.5

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 /*navbar hover menu */
 
+
+/*features-tabby*/
+
+
+/*FAQ__accordion*/
+
+
+
+
+/*
+### TODO ###
+1. finish footer
+2. fix the features-tabby (mainly positioning i think)
+3. create CSS for the navbar hover menu
+---
+4. create Javascript / functionality for navbar hover menu
+5. create Javascript / functionality for features-tabby
+6. create Javascript / functionality for FAQ__accordion
+--
+7. create tablet / desktop versions
+############
+*/
