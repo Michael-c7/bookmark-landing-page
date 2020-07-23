0bf9ef084be23721c55c48c136db5beba4fd662a
Michael Carr <mcarr2468@gmail.com>
2020-07-23 10:27:59 -0500

    v.1.5.3

--- script.js
+++ script.js
@@ -11,10 +11,17 @@
 
 /*
 ### TODO ###
-3. create CSS for the navbar hover menu
----
+1. create CSS for the navbar hover menu
+
+2. create active state for FAQ__accordion
+
+3. create active state for newsletter input
+ (use the attribute selector for this one[])
+-----
 4. create Javascript / functionality for navbar hover menu
+
 5. create Javascript / functionality for features-tabby
+
 6. create Javascript / functionality for FAQ__accordion
 --
 7. create tablet / desktop versions
