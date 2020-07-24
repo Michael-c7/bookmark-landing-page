7370be3f6fc64bc1ce967cc25b3580628a34f73d
Michael Carr <mcarr2468@gmail.com>
2020-07-23 19:37:37 -0500

    finsihed mobile version V1.0.0

--- script.js
+++ script.js
@@ -11,20 +11,24 @@
 
 /*
 ### TODO ###
-3. create CSS for the navbar hover menu 
 -----
-4. create Javascript functionality for navbar hover menu
+1. create Javascript functionality for navbar hover menu
 
-5. create Javascript functionality for features-tabby
+2. create Javascript functionality for features-tabby
 
-6. create Javascript functionality for FAQ__accordion
+3. create Javascript functionality for FAQ__accordion
 grab -> .FAQ__accordion__h4(listen for clicks on here)
  & .icon-arrow
  & .icon-arrow__arrow 
 (add the .FAQ__accordion--selected class to .icon-arrow__arrow)
 
-7. create javascript for newsletter input functionality
+4. create javascript for newsletter input functionality
+grab --> newsletter form &
+.newsletter__input (for the warning img & to read the input)
+& .newsletter__input-warning (for the message to slide down)
+apply .newsletter--warning-msg-on
+to both .newsletter__input & .newsletter__input-warning
 --
-8. create tablet / desktop versions
+5. create tablet / desktop versions
 ############
 */
