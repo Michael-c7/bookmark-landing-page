d594ba3918ea8298e58eaa004d98a529499f0e5b
Michael Carr <mcarr2468@gmail.com>
2020-07-23 21:07:06 -0500

    finished mobile version V1.0.1

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+let body = document.querySelector('body');
+let navbar = body.querySelector('.navbar');
+let navbarLogo = navbar.querySelector('.navbar__logo');
+
+let navbarLogoText = navbarLogo.querySelector('.navbar__logo__text');
+let navbarLogoBackground = navbarLogo.querySelector('.navbar__logo__background');
+let navbarLogoForeground = navbarLogo.querySelector('.navbar__logo__foreground');
+
+let navbarHamburgerMenu = navbar.querySelector('.navbar__hamburger-menu');
+let navbarHoverMenu = document.body.querySelector('.navbar__hover-menu');
+console.log(navbarHoverMenu)
+
 /*navbar hover menu */
+
+// 1. setup an event listener on the body 
+
+// 2. toggle the .menu-open class on .navbar__hover-menu
+
+// 2.5 toggle the .menu-open-navbar on
+// body, .navbar, .navbar__logo__text, 
+// .navbar__logo__background,
+// .navbar__logo__foreground
+
+// 2.75 toggle the icon from
+// src="help-files\images\icon-hamburger.svg"
+// TO
+// src="help-files\images\icon-close.svg
 
 
 /*features-tabby*/
@@ -15,6 +41,7 @@
 1. create Javascript functionality for navbar hover menu
 
 2. create Javascript functionality for features-tabby
+-
 
 3. create Javascript functionality for FAQ__accordion
 grab -> .FAQ__accordion__h4(listen for clicks on here)
