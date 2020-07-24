9ff015f44070367aa22bd1c58255d227bab9673e
Michael Carr <mcarr2468@gmail.com>
2020-07-24 10:36:05 -0500

    V1.0.2

--- script.js
+++ script.js
@@ -6,31 +6,44 @@ let navbarLogoText = navbarLogo.querySelector('.navbar__logo__text');
 let navbarLogoBackground = navbarLogo.querySelector('.navbar__logo__background');
 let navbarLogoForeground = navbarLogo.querySelector('.navbar__logo__foreground');
 
-let navbarHamburgerMenu = navbar.querySelector('.navbar__hamburger-menu');
 let navbarHoverMenu = document.body.querySelector('.navbar__hover-menu');
-console.log(navbarHoverMenu)
-
-/*navbar hover menu */
-
-// 1. setup an event listener on the body 
-
-// 2. toggle the .menu-open class on .navbar__hover-menu
-
-// 2.5 toggle the .menu-open-navbar on
-// body, .navbar, .navbar__logo__text, 
-// .navbar__logo__background,
-// .navbar__logo__foreground
-
-// 2.75 toggle the icon from
-// src="help-files\images\icon-hamburger.svg"
-// TO
-// src="help-files\images\icon-close.svg
+let navbarHamburgerMenu = navbar.querySelector('.navbar__hamburger-menu');
 
 
-/*features-tabby*/
+/*navbar hover menu*/
+function navbarHoverMenuFunctionality (event) {
+    // toggles the navbar hover menu
+    navbarHoverMenu.classList.toggle('menu-open');
+
+    // stuff in the navbar hover menu
+    body.classList.toggle('menu-open-navbar');
+    navbar.classList.toggle('menu-open-navbar');
+    // change the color of the company svg icon
+    navbarLogoText.classList.toggle('menu-open-navbar');
+    navbarLogoBackground.classList.toggle('menu-open-navbar');
+    navbarLogoForeground.classList.toggle('menu-open-navbar');
+
+    // toggles the hamburger menu / close button icon
+    if(navbarHoverMenu.classList.contains('menu-open')) {
+        event.target.src = 'help-files/images/icon-close.svg';
+    } else {
+        event.target.src = 'help-files/images/icon-hamburger.svg';
+    }
+}
+navbarHamburgerMenu.addEventListener('click', navbarHoverMenuFunctionality);
 
 
-/*FAQ__accordion*/
+
+// let featuresTabbyTitles = document.querySelector('.features-tabby__titles');
+// let featuresTabbyTitle = Array.from(document.querySelectorAll('.features-tabby__title'));
+
+/*features-tabby steps*/
+
+
+
+
+
+/*FAQ__accordion steps*/
 
 
 
@@ -38,8 +51,6 @@ console.log(navbarHoverMenu)
 /*
 ### TODO ###
 ------
-1. create Javascript functionality for navbar hover menu
-
 2. create Javascript functionality for features-tabby
 -
 
